95931b1a802f7147baf57f0294de8b3f24308950
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-05-02 18:10:25 +0000

    Showcase Work

--- src/main/java/org/primefaces/showcase/view/menu/MenuView.java
+++ src/main/java/org/primefaces/showcase/view/menu/MenuView.java
@@ -15,12 +15,60 @@
  */
 package org.primefaces.showcase.view.menu;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
 
 @ManagedBean
 public class MenuView {
+    
+    private MenuModel model;
+
+    @PostConstruct
+	public void init() {
+		model = new DefaultMenuModel();
+		
+		//First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+        
+        DefaultMenuItem item = new DefaultMenuItem("External");
+		item.setUrl("http://www.primefaces.org");
+        item.setIcon("ui-icon-home");
+		firstSubmenu.addElement(item);
+        
+        model.addElement(firstSubmenu);
+		
+		//Second submenu
+		DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+
+		item = new DefaultMenuItem("Save");
+		item.setIcon("ui-icon-disk");
+        item.setCommand("#{menuView.save}");
+        item.setUpdate("messages");
+		secondSubmenu.addElement(item);
+        
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("ui-icon-close");
+        item.setCommand("#{menuView.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+        
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("ui-icon-search");
+        item.setCommand("#{menuView.redirect}");
+		secondSubmenu.addElement(item);
+
+        model.addElement(secondSubmenu);
+	}
+
+	public MenuModel getModel() {
+		return model;
+	}	
     
     public void save() {
 		addMessage("Success", "Data saved");
