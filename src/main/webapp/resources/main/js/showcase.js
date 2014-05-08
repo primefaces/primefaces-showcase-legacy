743fefc2886d4368d9fbf44a29f203edad3ac6ef
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-05-08 11:30:55 +0000

    Showcase work

--- src/main/webapp/resources/main/js/showcase.js
+++ src/main/webapp/resources/main/js/showcase.js
@@ -1,5 +1,6 @@
 var winW=$(window).width();
 var winH=$(window).height();
+var whichMenuOpen="";
 
 $(document).ready(function() {
 	
@@ -23,22 +24,25 @@ $(document).ready(function() {
 	// menu mouseenter & mouseleave actions start ----------------------------------
 	$("#MENUSIDE").bind("mouseenter",function(){
 		$(this).clearQueue();
+        $(".MainLinkText").clearQueue();
 		$(this).animate({'width':450},500,"easeInOutQuint",function(){$(".MainLinkText").animate({'opacity':1,'margin-left':50},100);});
 		$("#LOGO").animate({'width':440},700,"easeInOutQuint");
+        $(".hiddenLogoText").animate({'opacity':1},600);
 		//$("#LOGOTEXTSIDE").animate({'margin-left':80},600,"easeInOutQuint");
 		$("#CONTENTSIDE").animate({'opacity':0.6});
 		$(".hiddenIcons").animate({'opacity':1},1000);
 		
 		$("#MENUSIDE").perfectScrollbar('update');
 		$("#CONTENTSIDE").perfectScrollbar('update');
-		$(".mustClose").slideDown(300);
+		$(".mustClose").slideDown(200);
 	});
 	$("#MENUSIDE").bind("mouseleave",function(){
 		$(".MainLinkText").clearQueue();
 		$(".MainLinkText").animate({'opacity':0,'margin-left':60},100);
 		$(this).clearQueue();
-		$(this).animate({'width':85},500,"easeInOutQuint");
+		$(this).animate({'width':85},700,"easeInOutQuint",function(){$(".MainLinkText").animate({'opacity':0,'margin-left':60},300);});
 		$("#LOGO").animate({'width':75},700,"easeInOutQuint");
+        $(".hiddenLogoText").animate({'opacity':0},600);
 		//$("#LOGOTEXTSIDE").animate({'margin-left':120},600,"easeInOutQuint");
 		$("#CONTENTSIDE").animate({'opacity':1});
 		$(".hiddenIcons").animate({'opacity':0},1000);
@@ -47,7 +51,7 @@ $(document).ready(function() {
 		$("#CONTENTSIDE").perfectScrollbar('update');
 		
 		$(".SubMenuLinkContainer").slideUp(700,"easeInOutQuint");
-		$(".mustClose").slideUp(300);
+		$(".mustClose").slideUp(200);
 	});
 	// -------------------------------------------------------------
 	
@@ -59,19 +63,31 @@ $(document).ready(function() {
 			$('.MOBILEHEADER').removeClass('fixedTop');
 		}
 	});
+    
+    // open theme switcher combo
+    $("#themeSwitcher").bind("mouseenter",function(){
+		$(".themeSwitchList").slideDown(500);
+	});
+	$("#themeSwitcher").bind("mouseleave",function(){
+		$(".themeSwitchList").slideUp(1);
+	});
 });
 
 
 // Open Submenu
-
 function openSubMenu(whichBtn){	
-	$(".MenuSideMainLink").removeClass("MenuSideMainLinkDark");
-	$(whichBtn).addClass("MenuSideMainLinkDark");
-	
-	$(".SubMenuLinkContainer").slideUp(700,"easeInOutQuint");
-	$(whichBtn).next().slideDown(700,"easeInOutQuint",function(){
-		//$("#MENUSIDE").animate({scrollTop: ($(whichBtn).offset().top)+100}, 1000);
-	});
+    $(".MenuSideMainLink").removeClass("MenuSideMainLinkDark");
+    $(whichBtn).addClass("MenuSideMainLinkDark");
+
+    if(whichMenuOpen==whichBtn){
+        $(".SubMenuLinkContainer").slideUp(700,"easeInOutQuint");
+        $(whichBtn).next().slideUp(700,"easeInOutQuint");
+        whichMenuOpen="";
+    }else{
+        $(".SubMenuLinkContainer").slideUp(700,"easeInOutQuint");
+        $(whichBtn).next().slideDown(700,"easeInOutQuint");
+        whichMenuOpen=whichBtn;
+    }
 }
 
 function windowSizeChanged(){
