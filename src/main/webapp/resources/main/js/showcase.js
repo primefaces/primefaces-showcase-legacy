820fbd75069458db399ccc0faf621f76ac8a1c8b
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-05-15 21:05:18 +0000

    Add news script only on index page

--- src/main/webapp/resources/main/js/showcase.js
+++ src/main/webapp/resources/main/js/showcase.js
@@ -146,15 +146,6 @@ $(document).ready(function() {
         });
 
     }
-    
-    //news
-    $.getJSON("http://blog.primefaces.org/?feed=json&jsonp=?", function(data) { 
-		var latestNewsContainer = $('#latestNews'),
-        entry = data[0];
-        
-        latestNewsContainer.prepend(entry.title);
-        latestNewsContainer.children('.dispBlock').html(entry.excerpt);			
-	});
 });
 
 // Open Submenu
