44b44ead2c03980bf7bbcf2b93388d133e2ff6df
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-05-21 11:31:26 +0000

    Demo update

--- showcase/trunk/src/main/java/org/primefaces/showcase/view/multimedia/PhotoCamView.java
+++ showcase/trunk/src/main/java/org/primefaces/showcase/view/multimedia/PhotoCamView.java
@@ -16,6 +16,7 @@
 package org.primefaces.showcase.view.multimedia;
 
 import java.io.File;
+import java.io.IOException;
 import javax.faces.FacesException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -54,7 +55,7 @@ public class PhotoCamView {
 			imageOutput.write(data, 0, data.length);
 			imageOutput.close();
 		}
-        catch(Exception e) {
+        catch(IOException e) {
 			throw new FacesException("Error in writing captured image.", e);
 		}
     }
