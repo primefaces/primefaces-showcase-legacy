e81e5e177861cfaec48af4eb801ede519491bcc0
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-06-08 21:13:40 +0000

    Demo update

--- showcase/trunk/src/main/java/org/primefaces/showcase/view/overlay/DialogView.java
+++ showcase/trunk/src/main/java/org/primefaces/showcase/view/overlay/DialogView.java
@@ -20,12 +20,17 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.event.CloseEvent;
+import org.primefaces.event.MoveEvent;
 
 @ManagedBean
 public class DialogView {
     
     public void handleClose(CloseEvent event) {
         addMessage(event.getComponent().getId() + " closed", "So you don't like nature?");
+    }
+    
+    public void handleMove(MoveEvent event) {
+        addMessage(event.getComponent().getId() + " moved", "Left: " + event.getLeft() + ", Top: " + event.getTop());
     }
     
     public void destroyWorld() {
