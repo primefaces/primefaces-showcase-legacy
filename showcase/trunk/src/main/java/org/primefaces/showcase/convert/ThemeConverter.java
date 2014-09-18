b5a223135256e7441ed5df7c2b370a8deb67dd14
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-09-18 08:56:31 +0000

    Demo update

--- showcase/trunk/src/main/java/org/primefaces/showcase/convert/ThemeConverter.java
+++ showcase/trunk/src/main/java/org/primefaces/showcase/convert/ThemeConverter.java
@@ -15,9 +15,11 @@
  */
 package org.primefaces.showcase.convert;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 import org.primefaces.showcase.domain.Theme;
@@ -28,8 +30,12 @@ public class ThemeConverter implements Converter {
 
     public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
         if(value != null && value.trim().length() > 0) {
-            ThemeService service = (ThemeService) fc.getExternalContext().getApplicationMap().get("themeService");
-            return service.getThemes().get(Integer.parseInt(value));
+            try {
+                ThemeService service = (ThemeService) fc.getExternalContext().getApplicationMap().get("themeService");
+                return service.getThemes().get(Integer.parseInt(value));
+            } catch(NumberFormatException e) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid theme."));
+            }
         }
         else {
             return null;
