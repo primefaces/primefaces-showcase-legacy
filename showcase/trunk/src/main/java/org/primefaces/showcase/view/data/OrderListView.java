fa2a7852c34167cb255e0048c034adb89c10fe6b
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-11-17 17:29:57 +0000

    Demo update

--- showcase/trunk/src/main/java/org/primefaces/showcase/view/data/OrderListView.java
+++ showcase/trunk/src/main/java/org/primefaces/showcase/view/data/OrderListView.java
@@ -18,8 +18,12 @@ package org.primefaces.showcase.view.data;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 
 import org.primefaces.showcase.service.ThemeService;
 import org.primefaces.showcase.domain.Theme;
@@ -72,4 +76,19 @@ public class OrderListView {
     public void setThemes(List<Theme> themes) {
         this.themes = themes;
     }    
+    
+    public void onSelect(SelectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", event.getObject().toString()));
+    }
+    
+    public void onUnselect(UnselectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Unselected", event.getObject().toString()));
+    }
+    
+    public void onReorder() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "List Reordered", null));
+    }
 }
