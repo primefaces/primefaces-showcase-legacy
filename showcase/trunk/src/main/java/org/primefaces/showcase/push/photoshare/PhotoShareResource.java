88294007da18d6df645ede2109f6558e17d52803
sincan.mert@gmail.com <sincan.mert@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-12-25 08:00:26 +0000

    added PhotoShare demo for PrimeFaces Push

---
+++ showcase/trunk/src/main/java/org/primefaces/showcase/push/photoshare/PhotoShareResource.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2009-2014 PrimeTek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.showcase.push.photoshare;
+
+import org.primefaces.push.annotation.OnMessage;
+import org.primefaces.push.annotation.PushEndpoint;
+import org.primefaces.push.impl.JSONEncoder;
+
+@PushEndpoint("/photoshare")
+public class PhotoShareResource {
+    
+    @OnMessage(encoders = {JSONEncoder.class})
+    public String onMessage(String filename) {
+        return filename;
+    }
+}
