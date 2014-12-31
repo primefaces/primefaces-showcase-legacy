0b1e3fe45f2461ab2319013d37a8ca2d93ca5f1a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-12-31 12:32:49 +0000

    Kickstart sample for new diagram component

---
+++ src/main/java/org/primefaces/showcase/view/data/diagram/BasicView.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2009-2014 PrimeTek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.showcase.view.data.diagram;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.primefaces.model.diagram.DefaultDiagramModel;
+import org.primefaces.model.diagram.DiagramModel;
+import org.primefaces.model.diagram.Element;
+
+@ManagedBean(name = "diagramBasicView")
+@RequestScoped
+public class BasicView {
+    
+    private DiagramModel model;
+
+    @PostConstruct
+    public void init() {
+        model = new DefaultDiagramModel();
+        model.addElement(new Element("A", "20em", "6em"));
+        model.addElement(new Element("B", "10em", "18em"));
+        model.addElement(new Element("C", "40em", "18em"));
+    }
+    
+    public DiagramModel getModel() {
+        return model;
+    }
+}
