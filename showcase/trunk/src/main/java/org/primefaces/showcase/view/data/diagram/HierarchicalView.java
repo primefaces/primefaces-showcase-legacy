6efa3546efe86e78eb9ed33e7d4e825dd8f90503
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2015-01-07 17:26:31 +0000

    Demo update

--- showcase/trunk/src/main/java/org/primefaces/showcase/view/data/diagram/HierarchicalView.java
+++ showcase/trunk/src/main/java/org/primefaces/showcase/view/data/diagram/HierarchicalView.java
@@ -23,6 +23,7 @@ import org.primefaces.model.diagram.DiagramModel;
 import org.primefaces.model.diagram.Element;
 import org.primefaces.model.diagram.Connection;
 import org.primefaces.model.diagram.connector.StraightConnector;
+import org.primefaces.model.diagram.endpoint.EndPoint;
 import org.primefaces.model.diagram.endpoint.EndPointAnchor;
 import org.primefaces.model.diagram.endpoint.RectangleEndPoint;
 
@@ -36,21 +37,21 @@ public class HierarchicalView {
     public void init() {
         model = new DefaultDiagramModel();
         model.setMaxConnections(-1);
-        
+                
         Element ceo = new Element("CEO", "25em", "6em");
-        ceo.addEndPoint(new RectangleEndPoint(EndPointAnchor.BOTTOM));
+        ceo.addEndPoint(createEndPoint(EndPointAnchor.BOTTOM));
         model.addElement(ceo);
         
         //CFO
         Element cfo = new Element("CFO", "10em", "18em");
-        cfo.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
-        cfo.addEndPoint(new RectangleEndPoint(EndPointAnchor.BOTTOM));
+        cfo.addEndPoint(createEndPoint(EndPointAnchor.TOP));
+        cfo.addEndPoint(createEndPoint(EndPointAnchor.BOTTOM));
         
         Element fin = new Element("FIN", "5em", "30em");
-        fin.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
+        fin.addEndPoint(createEndPoint(EndPointAnchor.TOP));
         
         Element pur = new Element("PUR", "20em", "30em");
-        pur.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
+        pur.addEndPoint(createEndPoint(EndPointAnchor.TOP));
         
         model.addElement(cfo);
         model.addElement(fin);
@@ -58,26 +59,38 @@ public class HierarchicalView {
         
         //CTO
         Element cto = new Element("CTO", "40em", "18em");
-        cto.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
-        cto.addEndPoint(new RectangleEndPoint(EndPointAnchor.BOTTOM));
+        cto.addEndPoint(createEndPoint(EndPointAnchor.TOP));
+        cto.addEndPoint(createEndPoint(EndPointAnchor.BOTTOM));
         
         Element dev = new Element("DEV", "35em", "30em");
-        dev.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
+        dev.addEndPoint(createEndPoint(EndPointAnchor.TOP));
         
         Element tst = new Element("TST", "50em", "30em");
-        tst.addEndPoint(new RectangleEndPoint(EndPointAnchor.TOP));
+        tst.addEndPoint(createEndPoint(EndPointAnchor.TOP));
         
         model.addElement(cto);
         model.addElement(dev);
         model.addElement(tst);
+        
+        StraightConnector connector = new StraightConnector();
+        connector.setPaintStyle("{strokeStyle:'#404a4e', lineWidth:3}");
+        connector.setHoverPaintStyle("{strokeStyle:'#20282b'}");
                         
         //connections
-        model.connect(new Connection(ceo.getEndPoints().get(0), cfo.getEndPoints().get(0), new StraightConnector()));        
-        model.connect(new Connection(ceo.getEndPoints().get(0), cto.getEndPoints().get(0), new StraightConnector()));
-        model.connect(new Connection(cfo.getEndPoints().get(1), fin.getEndPoints().get(0), new StraightConnector()));
-        model.connect(new Connection(cfo.getEndPoints().get(1), pur.getEndPoints().get(0), new StraightConnector()));
-        model.connect(new Connection(cto.getEndPoints().get(1), dev.getEndPoints().get(0), new StraightConnector()));
-        model.connect(new Connection(cto.getEndPoints().get(1), tst.getEndPoints().get(0), new StraightConnector()));
+        model.connect(new Connection(ceo.getEndPoints().get(0), cfo.getEndPoints().get(0), connector));        
+        model.connect(new Connection(ceo.getEndPoints().get(0), cto.getEndPoints().get(0), connector));
+        model.connect(new Connection(cfo.getEndPoints().get(1), fin.getEndPoints().get(0), connector));
+        model.connect(new Connection(cfo.getEndPoints().get(1), pur.getEndPoints().get(0), connector));
+        model.connect(new Connection(cto.getEndPoints().get(1), dev.getEndPoints().get(0), connector));
+        model.connect(new Connection(cto.getEndPoints().get(1), tst.getEndPoints().get(0), connector));
+    }
+    
+    private EndPoint createEndPoint(EndPointAnchor anchor) {
+        RectangleEndPoint endPoint = new RectangleEndPoint(anchor);
+        endPoint.setStyle("{fillStyle:'#404a4e'}");
+        endPoint.setHoverStyle("{fillStyle:'#20282b'}");
+        
+        return endPoint;
     }
     
     public DiagramModel getModel() {
