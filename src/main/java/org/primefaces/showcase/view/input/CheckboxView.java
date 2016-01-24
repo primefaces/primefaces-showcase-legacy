d9ced6e2e971fa5da6388ae93e5138d2e5a1e584
avereco <avereco@gmail.com>
2016-01-24 21:49:43 +0200

    manyCheckbox support for grouped items

--- src/main/java/org/primefaces/showcase/view/input/CheckboxView.java
+++ src/main/java/org/primefaces/showcase/view/input/CheckboxView.java
@@ -19,16 +19,19 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 
 @ManagedBean
 public class CheckboxView {
-    
-    private String[] selectedConsoles;  
+
+    private String[] selectedConsoles;
     private String[] selectedConsoles2;
     private String[] selectedCities;
     private String[] selectedCities2;
     private List<String> cities;
-    
+    private List<SelectItem> cars;
+
     @PostConstruct
     public void init() {
         cities = new ArrayList<String>();
@@ -41,6 +44,23 @@ public class CheckboxView {
         cities.add("Rome");
         cities.add("Brasilia");
         cities.add("Amsterdam");
+        cars = new ArrayList<SelectItem>();
+        SelectItemGroup germanCars = new SelectItemGroup("German Cars");
+        germanCars.setSelectItems(new SelectItem[]{
+            new SelectItem("BMW", "BMW"),
+            new SelectItem("Mercedes", "Mercedes"),
+            new SelectItem("Volkswagen", "Volkswagen")
+        });
+
+        SelectItemGroup americanCars = new SelectItemGroup("American Cars");
+        americanCars.setSelectItems(new SelectItem[]{
+            new SelectItem("Chrysler", "Chrysler"),
+            new SelectItem("GM", "GM"),
+            new SelectItem("Ford", "Ford")
+        });
+
+        cars.add(germanCars);
+        cars.add(americanCars);
     }
 
     public String[] getSelectedConsoles() {
@@ -66,7 +86,7 @@ public class CheckboxView {
     public void setSelectedCities2(String[] selectedCities2) {
         this.selectedCities2 = selectedCities2;
     }
-    
+
     public String[] getSelectedConsoles2() {
         return selectedConsoles2;
     }
@@ -74,8 +94,17 @@ public class CheckboxView {
     public void setSelectedConsoles2(String[] selectedConsoles2) {
         this.selectedConsoles2 = selectedConsoles2;
     }
-    
+
     public List<String> getCities() {
         return cities;
     }
+
+    public List<SelectItem> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<SelectItem> cars) {
+        this.cars = cars;
+    }
+
 }
