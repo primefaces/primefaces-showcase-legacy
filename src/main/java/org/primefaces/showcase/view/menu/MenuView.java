1f4bf0afe373cea601fc0cf0346fc6caf6e273ef
Rapster <sebastien.lepage@outlook.com>
2018-04-10 23:55:42 +0200

    Fix #3563

--- src/main/java/org/primefaces/showcase/view/menu/MenuView.java
+++ src/main/java/org/primefaces/showcase/view/menu/MenuView.java
@@ -19,6 +19,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.DefaultSubMenu;
@@ -26,64 +27,76 @@ import org.primefaces.model.menu.MenuModel;
 
 @ManagedBean
 public class MenuView {
-    
+
     private MenuModel model;
 
     @PostConstruct
-	public void init() {
-		model = new DefaultMenuModel();
-		
-		//First submenu
-        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
-        
-        DefaultMenuItem item = new DefaultMenuItem("External");
-		item.setUrl("http://www.primefaces.org");
-        item.setIcon("ui-icon-home");
-		firstSubmenu.addElement(item);
-        
-        model.addElement(firstSubmenu);
-		
-		//Second submenu
-		DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+    public void init() {
+        model = new DefaultMenuModel();
 
-		item = new DefaultMenuItem("Save");
-		item.setIcon("ui-icon-disk");
-        item.setCommand("#{menuView.save}");
-        item.setUpdate("messages");
-		secondSubmenu.addElement(item);
-        
-        item = new DefaultMenuItem("Delete");
-        item.setIcon("ui-icon-close");
-        item.setCommand("#{menuView.delete}");
-        item.setAjax(false);
+        //First submenu
+        DefaultSubMenu firstSubmenu = DefaultSubMenu.builder()
+                .withLabel("Dynamic Submenu")
+                .build();
+
+        DefaultMenuItem item = DefaultMenuItem.builder()
+                .withValue("External")
+                .withUrl("http://www.primefaces.org")
+                .withIcon("ui-icon-home")
+                .build();
+        firstSubmenu.addElement(item);
+
+        model.addElement(firstSubmenu);
+
+        //Second submenu
+        DefaultSubMenu secondSubmenu = DefaultSubMenu.builder()
+                .withLabel("Dynamic Actions")
+                .build();
+
+        item = DefaultMenuItem.builder()
+                .withValue("Save")
+                .withIcon("ui-icon-disk")
+                .withCommand("#{menuView.save}")
+                .withUpdate("messages")
+                .build();
         secondSubmenu.addElement(item);
-        
-        item = new DefaultMenuItem("Redirect");
-        item.setIcon("ui-icon-search");
-        item.setCommand("#{menuView.redirect}");
-		secondSubmenu.addElement(item);
+
+        item = DefaultMenuItem.builder()
+                .withValue("Delete")
+                .withIcon("ui-icon-close")
+                .withCommand("#{menuView.delete}")
+                .withAjax(false)
+                .build();
+        secondSubmenu.addElement(item);
+
+        item = DefaultMenuItem.builder()
+                .withValue("Redirect")
+                .withIcon("ui-icon-search")
+                .withCommand("#{menuView.redirect}")
+                .build();
+        secondSubmenu.addElement(item);
 
         model.addElement(secondSubmenu);
-	}
+    }
 
-	public MenuModel getModel() {
-		return model;
-	}	
-    
+    public MenuModel getModel() {
+        return model;
+    }
+
     public void save() {
-		addMessage("Success", "Data saved");
-	}
-	
-	public void update() {
-		addMessage("Success", "Data updated");
-	}
-	
-	public void delete() {
-		addMessage("Success", "Data deleted");
-	}
-	
-	public void addMessage(String summary, String detail) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	}
+        addMessage("Success", "Data saved");
+    }
+
+    public void update() {
+        addMessage("Success", "Data updated");
+    }
+
+    public void delete() {
+        addMessage("Success", "Data deleted");
+    }
+
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 }
